a5660c186f7f1432091d9a686fea06c71d714729
Nicolás Cáceres Sala <ncsala@gmail.com>
2024-05-09 22:28:57 -0300

    chore: remove code

--- BinarySearchTrees/bst_construction.py
+++ BinarySearchTrees/bst_construction.py
@@ -6,7 +6,24 @@ class BST:
   
   def __repr__(self):
     return f"BST(root={self.root}, left={self.left}, right={self.right})"
+  
+  def insert(self, value):
+    if value > self.root:
+      if self.right is None:
+        self.right = BST(value)
+      else:
+        self.right.insert(value)
+    elif value < self.root:
+      if self.left is None:
+        self.left = BST(value)
+      else:
+        self.left.insert(value)
+        
 
-bst1 = BST(2)
+bst = BST(2)
+bst.insert(15)
+bst.insert(5)
+bst.insert(12)
+bst.insert(3)
 
-print(bst1)
+print(bst)
--- BinarySearchTrees/right_smaller_than.py
+++ BinarySearchTrees/right_smaller_than.py
@@ -12,25 +12,5 @@ def right_smaller_than(array):
         result.append(count_smaller)
     
     return result
-
-print(right_smaller_than([8,5,11,-1,3,4,2]))
-
-
-# 
-def right_smaller_than2(array):
-  if len(array) == 0:
-    return []
   
-    lastIndex = len(array) - 1
-    bst = special_BST(array[lastIdx], lastIdx, 0)
-    
-
-
-
-class special_BST():
-  def __init__(self, value, idx, numSmallerAtInsertTime):
-    pass
-    
-    
-    
-    
+print(right_smaller_than([8,5,11,-1,3,4,2]))
